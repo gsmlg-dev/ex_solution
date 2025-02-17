981a4994f4878785e816a0c36983ba10acb8e2d5
gaoshiming <gaoshiming@zdns.cn>
2025-02-17 11:37:39 +0800

    feat: Init.

---
+++ lib/gsmlg/solution.ex
@@ -0,0 +1,18 @@
+defmodule GSMLG.Solution do
+  @moduledoc """
+  Documentation for `GSMLG.Solution`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GSMLG.Solution.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gsmlg/solution_test.exs
@@ -0,0 +1,8 @@
+defmodule GSMLG.SolutionTest do
+  use ExUnit.Case
+  doctest GSMLG.Solution
+
+  test "greets the world" do
+    assert GSMLG.Solution.hello() == :world
+  end
+end
